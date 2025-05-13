d01307b15f3589ff9b260113581f46da65784ad7
Abhilash John <abhilashjohn07@gmail.com>
2025-05-13 22:12:16 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -227,19 +227,18 @@ const config = {
         "@gracefullight/docusaurus-plugin-google-adsense",
         { adClient: "ca-pub-9381464195828471" },
       ],
-      
       [
         '@docusaurus/plugin-google-gtag',
         {
           trackingID: 'GTM-KLGV623', // Replace with your Google Analytics tracking ID
           anonymizeIP: false, // Optional: Enables IP anonymization
         },
-
-        '@docusaurus/@docusaurus/plugin-google-tag-manager',
+      ],
+      [
+        '@docusaurus/plugin-google-tag-manager',
         {
           containerId: 'GTM-KLGV623',
         },
-
       ],
     ],
     // End of plugin export code
